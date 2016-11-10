3e720203776b8150a4d77520f67cb825c02637d3
DESKTOP-08PUL5A\trung <trungvngo@gmail.com>
2016-11-09 19:23:17 -0800

    setup files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,14 @@
 // ========== guidebox api =========
 
-var guideBoxApiKey = 'TeAuj5W9pJ2iy8G4xcz30fhV0yqSqz';
+
+$('#submit-btn').on('click', function(){
+	var guideBoxApiKey = 'TeAuj5W9pJ2iy8G4xcz30fhV0yqSqz';
+	var movieSearch = $('#user-search').val().trim();
+	var movieQuery = "https://api-public.guidebox.com/v1.43/US/" + guideBoxApiKey + "/search/movie/title/" + movieSearch + "/fuzzy";
+	console.log(movieSearch);
+	$(document).ready(function(){
+		$.ajax({url: movieQuery, method: 'GET'}).done(function(data){
+			console.log(data);
+		});
+	});
+});
