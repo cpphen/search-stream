046d6b0d6d49b053ea3bc737b18d7c530f7cdd4b
DESKTOP-08PUL5A\trung <trungvngo@gmail.com>
2016-11-12 13:33:52 -0800

    added display of results on click

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,69 +1,99 @@
-// ========== guidebox api =========
+/* ================ result component ==============
 
-var guideBoxApiKey = 'rKqEl4tThhp94JtuWhfE3ECq1EsgQQIR';
-var idList = [];
-var movieData = [];
+	<div class="row result">
+		<!-- movie image -->
+		<div class="col-lg-3 movie-poster">
+			<img src="http://static.rogerebert.com/uploads/movie/movie_poster/nightcrawler-2014/large_ciARJx8fJqum8uh6gLePaNoyYNY.jpg" alt="">
+		</div>
+		<!-- movie description & ratings -->
+		<div class="col-lg-6 movie-description">
+			<h2>Description	</h2>
+			<p>When Louis Bloom, a driven man desperate for work, muscles into the world of L.A. crime journalism, he blurs the line between observer and participant to become the star of his own story. Aiding him in his effort is Nina, a TV-news veteran.</p>
+			<h2>Ratings</h2>			
+			<ul class="reviews">
+				<li>Metacritic</li>
+				<li>Rotten Tomatoes</li>
+			</ul>
+		</div>
+		<!-- streaming links -->
+		<div class="col-lg-3 streaming-links">
+			<h2>Streaming On</h2>
+			<!-- displays available streaming sources, or none if there aren't any available -->
+			<ul>
+				<li>Netflix</li>
+				<li>HBO GO</li>
+				<li>Amazon Prime</li>
+			</ul>
+		</div>
+	</div>
+
+================================================= */ 
 
 $('#submit-btn').on('click', function(){
-	var movieSearch = $('#user-search').val().trim();
-	var movieQuery = "https://api-public.guidebox.com/v1.43/US/" + guideBoxApiKey + "/search/movie/title/" + movieSearch + "/fuzzy";
-	
-	$.ajax({url: movieQuery, method: 'GET'}).done(function(data){
-		// log the query url for reference
-		console.log(movieQuery);
-		
-		// get result length to use as a stopping point in condition of for loop
-		var resultLength = data.results.length;
-
-		// loop through results to get back the movie id
-		for(var i = 0; i < resultLength; i++) {
-			var id = data.results[i].id;
-			// add the id to the idList array
-			idList.push(id);
-		} 
-
-		// function is a separate api call for each movie to get more specific movie data
-		movieIDSearch();
-	});
+	reset();
+	getMovieData();
 });
 
+$(document).ajaxStop(function() {
+	getResults();
+});
 
-function movieIDSearch(){
-	for(var i = 0; i < idList.length; i++){	
-			// get movie id
-			var movieId = idList[i];
-			
-			// construct api request
-			var movieIdQuery = "https://api-public.guidebox.com/v1.43/US/" + guideBoxApiKey + "/movie/" + movieId;
-			
-			// search movie id using fully constructed movieIdQuery  
-			movieIdAjax(movieIdQuery);
+function reset(){
+	// empty id list
+	idList = [];
+	// empty movieData obj
+	movieData = [];
+	// clear results display
+	$('#results').empty();
+}
+
+function getResults() {	
+	// select results section to append everything to
+	var results = $('#results');
+	console.log("in get results function: " + movieData);
+	for(var i = 0; i < movieData.length; i++) {
+		
+		// // construct a div with class = 'row result'
+		var resultDiv = $('<div>').addClass('row result');
+
+		// // construct image div with col-lg-3 and movie-poster class
+		var imageDiv = $('<div>').addClass('col-lg-3 movie-poster');
+		// // construct image element
+		var image = $('<img>').attr('src',movieData[i].imageLink);
+		// // append image elem to div
+		imageDiv.append(image);
+
+		// // construct movie description and rating div
+		var descDiv = $('<div>').addClass('col-lg-6 movie-description');
+		var descriptionHeading = $('<h2>').text('Description');
+		var description = $('<p>').text(movieData[i].description);
+		// // append heading and description
+		descDiv.append(descriptionHeading);
+		descDiv.append(description);
+
+		// // reviews
+		var reviewHeading = $('<h2>').text('Ratings');
+		var reviewUl = $('<ul>').addClass('reviews');
+		var reviewLi = $('<li>');
+		// // append review heading to description and ratings div
+		descDiv.append(reviewHeading);
+		// // append ratings that exist
+
+		// // streaming links div
+		var streamingDiv = $('<div>').addClass('col-lg-3 streaming-links');
+		var streamingHeading = $('<h2>').text('Streaming On');
+		var streamingUl = $('<ul>');
+		var streamingLi = $('<li>');
+		// // append to streamingDiv
+		streamingDiv.append(streamingHeading);
+		// // append streaming sources that exist
+
+		// // append all constructed columns to row div
+		resultDiv.append(imageDiv);
+		resultDiv.append(descDiv);
+		resultDiv.append(streamingDiv);
+
+		// // append completed result component to results display section
+		results.append(resultDiv);
 	}
 }
-
-function movieIdAjax(movieIdQuery) {
-	$.ajax({url: movieIdQuery, method: 'GET'}).done(function(data){
-		// store movie data
-		var id = data.id;
-		var title = data.title;
-		var releaseYear = data.release_year;
-		var description = data.overview;
-		var imageLink = data.poster_240x342;
-		var streamingSources = data.subscription_web_sources;
-
-		// construct obj with the var references
-		var movieObj = {
-			id: id,
-			title: title,
-			releaseYear: releaseYear,
-			description: description,
-			imageLink: imageLink,
-			streamingSources: streamingSources
-		}
-
-		// add obj to the movieData array
-		movieData.push(movieObj);
-
-	});	
-}
-
