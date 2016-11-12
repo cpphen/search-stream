df8d04b5bcdc7fb02edef4dd3a3a135503fdaea5
DESKTOP-08PUL5A\trung <trungvngo@gmail.com>
2016-11-11 17:12:09 -0800

    hardcoded result component, guidebox api setup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,55 @@
 // ========== guidebox api =========
 
+var idList = [];
+var movieTitles = [];
 
 $('#submit-btn').on('click', function(){
-	var guideBoxApiKey = 'TeAuj5W9pJ2iy8G4xcz30fhV0yqSqz';
+	var guideBoxApiKey = 'rKqEl4tThhp94JtuWhfE3ECq1EsgQQIR';
 	var movieSearch = $('#user-search').val().trim();
 	var movieQuery = "https://api-public.guidebox.com/v1.43/US/" + guideBoxApiKey + "/search/movie/title/" + movieSearch + "/fuzzy";
-	console.log(movieSearch);
-	$(document).ready(function(){
-		$.ajax({url: movieQuery, method: 'GET'}).done(function(data){
-			console.log(data);
-		});
+	
+	$.ajax({url: movieQuery, method: 'GET'}).done(function(data){
+		// console.log(data);
+
+		for(var i = 0; i < 10; i++) {
+			var id = data.results[i].id;
+			idList.push(id);
+		} 
+
+		movieIDSearch();
 	});
+
 });
+
+
+function movieIDSearch(){
+	for(var i = 0; i < idList.length; i++){	
+			var guideBoxApiKey =  'rKqEl4tThhp94JtuWhfE3ECq1EsgQQIR';
+			// get movie id
+			var movieId = idList[i];
+			
+			// construct api request
+			var movieIdQuery = "https://api-public.guidebox.com/v1.43/US/" + guideBoxApiKey + "/movie/" + movieId;
+			
+			// search movie id using fully constructed movieIdQuery  
+			movieIdAjax(movieIdQuery);
+	}
+}
+
+function movieIdAjax(movieIdQuery) {
+	$.ajax({url: movieIdQuery, method: 'GET'}).done(function(data){
+		// store movie data
+		var movieTitle = data.title;
+		movieTitles.push(movieTitle);
+	});	
+}
+
+// console.log(movieTitles);
+
+// for (var i .....) {
+//   (function (i) {
+//     async(function() {
+//       use(i);
+//     });
+//   })(i);
+// }
