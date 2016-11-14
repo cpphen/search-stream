742a7ff7037e9fa8e3039cc39d1d693a10130163
DESKTOP-08PUL5A\trung <trungvngo@gmail.com>
2016-11-13 19:14:16 -0800

    added styles

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,10 +32,18 @@
 $('#submit-btn').on('click', function(){
 	reset();
 	getMovieData();
+	// display loading animated gif
+	$('#loading').removeClass('hide');
 });
 
 $(document).ajaxStop(function() {
 	getResults();
+	// remove loading animate gif
+	$('#loading').addClass('hide');
+	// animate height of search section to auto
+	$('.container-fluid').css('height','auto');
+	// display the results section
+	$('.container').removeClass('hide');
 });
 
 function reset(){
@@ -53,47 +61,77 @@ function getResults() {
 
 	for(var i = 0; i < movieData.length; i++) {
 		
-		// // construct a div with class = 'row result'
+		// construct a div with class = 'row result'
 		var resultDiv = $('<div>').addClass('row result');
 
-		// // construct image div with col-lg-3 and movie-poster class
-		var imageDiv = $('<div>').addClass('col-lg-3 movie-poster');
-		// // construct image element
+		// construct image div with col-lg-3 and movie-poster class
+		var imageDiv = $('<div>').addClass('col-sm-6 col-lg-3 movie-poster');
+		// construct image element
 		var image = $('<img>').attr('src',movieData[i].imageLink);
-		// // append image elem to div
+		// append image elem to div
 		imageDiv.append(image);
 
-		// // construct movie description and rating div
-		var descDiv = $('<div>').addClass('col-lg-6 movie-description');
+		// construct movie description and rating div
+		var descDiv = $('<div>').addClass('col-sm-6 col-lg-6 movie-description');
 		var descriptionHeading = $('<h2>').text('Description');
 		var description = $('<p>').text(movieData[i].description);
-		// // append heading and description
+		// append heading and description
 		descDiv.append(descriptionHeading);
 		descDiv.append(description);
 
-		// // reviews
+		// reviews
 		var reviewHeading = $('<h2>').text('Ratings');
 		var reviewUl = $('<ul>').addClass('reviews');
 		var reviewLi = $('<li>');
-		// // append review heading to description and ratings div
+		// append review heading to description and ratings div
 		descDiv.append(reviewHeading);
-		// // append ratings that exist
+		// append ratings that exist
 
-		// // streaming links div
-		var streamingDiv = $('<div>').addClass('col-lg-3 streaming-links');
+		// streaming links div
+		var streamingDiv = $('<div>').addClass('col-sm-12 col-lg-3 streaming-links');
 		var streamingHeading = $('<h2>').text('Streaming On');
 		var streamingUl = $('<ul>');
-		var streamingLi = $('<li>');
-		// // append to streamingDiv
-		streamingDiv.append(streamingHeading);
-		// // append streaming sources that exist
+		var noStreams = $('<div>');
 
-		// // append all constructed columns to row div
+		// loop through streaming sources and create li
+		if(movieData[i].streamingSources.length === 0) {
+			var notStreaming = $('<p>').text('Not Currently Streaming');
+			var signupBtn = $('<button>').text("Notify Me When It's Streaming");
+			signupBtn.addClass('signup-btn');
+			noStreams.append(notStreaming);
+			noStreams.append(signupBtn);
+			
+		} else {
+			for (var x  = 0; x < movieData[i].streamingSources.length; x++){
+				var streamingLi = $('<li>');
+				// get streaming source display name
+				var source = movieData[i].streamingSources[x].display_name;
+				// append text to li
+				streamingLi.text(source);
+				// append li to ul
+				streamingUl.append(streamingLi);
+			}
+		}
+
+		// append to streamingDiv
+		streamingDiv.append(streamingHeading);
+		
+		// append streaming sources that exist, or show not streaming message
+		if(movieData[i].streamingSources.length === 0) {
+			streamingDiv.append(noStreams);
+		} else {			
+			streamingDiv.append(streamingUl);
+		}
+
+		// append all constructed columns to row div
 		resultDiv.append(imageDiv);
 		resultDiv.append(descDiv);
 		resultDiv.append(streamingDiv);
 
-		// // append completed result component to results display section
+		// append completed result component to results display section
 		results.append(resultDiv);
 	}
 }
+
+// get length of streaming sources: movieData[0].streamingSources.length
+// get streaming sources display name: movieData[0].streamingSources[0].display_name
